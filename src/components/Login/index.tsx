284c03231ff777b4a32cef1aec40fc3790170dc2
Jo-Jee <capjjo@gmail.com>
2022-12-16 18:32:41 +0900

    password error 삭제

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -55,16 +55,13 @@ function Login() {
               Password
             </label>
             <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               name="password"
               type="password"
               placeholder="******************"
               value={password}
               onChange={onChange}
             />
-            <p className="text-red-500 text-xs italic">
-              Please choose a password.
-            </p>
           </div>
           <div>
             <button
