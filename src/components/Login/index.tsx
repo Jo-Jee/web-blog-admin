d49c0d6fe1f74f7564c3384d3d9122c169e8877a
Jo-Jee <capjjo@gmail.com>
2022-12-17 09:41:20 +0900

    로그인 토큰 localstorage 저장 설정

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -22,20 +22,28 @@ function Login() {
     })
   }
 
-  const login = async () => {
-    const res = await axios.post<LoginRes>('http://localhost:8080', {
-      email: email,
-      password: password,
-    })
+  const login = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const res = await axios.post<LoginRes>(
+      'http://localhost:8080/api/v1/auth/login',
+      {
+        email: email,
+        password: password,
+      }
+    )
 
+    localStorage.setItem('accessToken', res.data.accessToken)
+    localStorage.setItem('refreshToken', res.data.refreshToken)
     dispatch(userActions.setToken(res.data))
-    console.log(email, password)
   }
 
   return (
     <div className="flex justify-center mt-20">
       <div className="w-full max-w-xs">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          onSubmit={login}
+        >
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
               Email
@@ -64,11 +72,7 @@ function Login() {
             />
           </div>
           <div>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-              type="button"
-              onClick={() => login()}
-            >
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full">
               로그인
             </button>
           </div>
