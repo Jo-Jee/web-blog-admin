2ac14e1ac436ca8f94ce6c4bc3cabcd8f57b9d25
Jo-Jee <capjjo@gmail.com>
2022-12-23 17:45:22 +0900

    포스트 페이징 기능 추가

--- src/pages/ManagePosts.tsx
+++ src/pages/ManagePosts.tsx
@@ -8,26 +8,55 @@ interface PostTableRowProps {
   post: Post
 }
 
+interface pageInfoState {
+  curPage: number
+  totalPage: number
+  pageSize: number
+  pageNavIndex: number[]
+  posts: Post[]
+  selectedPost?: Post
+}
+
 export default function ManagePosts() {
-  const [page, setPage] = useState(0)
-  const [posts, setPosts] = useState<Array<Post>>([])
-  const [selectedPost, setSelectedPost] = useState<Post>()
-  const pageSize = 10
+  const [pageInfo, setPageInfo] = useState<pageInfoState>({
+    curPage: 0,
+    totalPage: 0,
+    pageSize: 10,
+    pageNavIndex: [],
+    posts: [],
+    selectedPost: undefined,
+  })
+  const { curPage, totalPage, pageSize, pageNavIndex, posts, selectedPost } =
+    pageInfo
 
   useEffect(() => {
     async function getPosts() {
+      let navPagesIndexes: number[] = []
+      let basePage = curPage - 2
+
       const res = await API.blog.get('/posts', {
         params: {
-          page: page,
+          page: curPage,
           size: pageSize,
         },
       })
 
-      setPosts(res.data.content)
+      while (navPagesIndexes.length < 5) {
+        if (basePage === res.data.totalPages) break
+        if (basePage > -1) navPagesIndexes.push(basePage)
+        basePage++
+      }
+
+      setPageInfo({
+        ...pageInfo,
+        posts: res.data.content,
+        totalPage: res.data.totalPages,
+        pageNavIndex: navPagesIndexes,
+      })
     }
 
     getPosts()
-  }, [page])
+  }, [pageInfo.curPage])
 
   return (
     <div>
@@ -44,22 +73,91 @@ export default function ManagePosts() {
 
   function PostTable() {
     return (
-      <table className="table-auto w-full">
-        <thead>
-          <tr className="border-b">
-            <th className="px-6 py-2 text-left w-10">ID</th>
-            <th className="px-6 py-2 text-left w-80">Title</th>
-            <th className="px-6 py-2 text-left">Create Time</th>
-            <th className="px-6 py-2 text-left">Update Time</th>
-            <th className="px-6 py-2 text-left w-20">Pub</th>
-          </tr>
-        </thead>
-        <tbody>
-          {posts.map((post) => (
-            <PostTableRow post={post} key={post.id} />
-          ))}
-        </tbody>
-      </table>
+      <div>
+        <table className="table-auto w-full">
+          <thead>
+            <tr className="border-b">
+              <th className="px-6 py-2 text-left w-10">ID</th>
+              <th className="px-6 py-2 text-left w-80">Title</th>
+              <th className="px-6 py-2 text-left">Create Time</th>
+              <th className="px-6 py-2 text-left">Update Time</th>
+              <th className="px-6 py-2 text-left w-20">Pub</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map((post) => (
+              <PostTableRow post={post} key={post.id} />
+            ))}
+          </tbody>
+        </table>
+        <div className="flex justify-center">
+          <nav>
+            <div className="inline-flex items-center -space-x-px">
+              <span>
+                <button
+                  onClick={() => {
+                    setPageInfo({
+                      ...pageInfo,
+                      curPage: Math.max(0, curPage - 1),
+                    })
+                  }}
+                  className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"></path>
+                  </svg>
+                </button>
+              </span>
+              {pageNavIndex.map((index) => (
+                <span key={index}>
+                  <button
+                    onClick={() => {
+                      setPageInfo({
+                        ...pageInfo,
+                        curPage: index,
+                      })
+                    }}
+                    className={`px-3 py-2 leading-tight ${
+                      index === curPage
+                        ? 'relative z-10 text-sky-600 bg-sky-50 border-sky-500 hover:text-sky-700 hover:bg-sky-100'
+                        : 'text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700'
+                    } border `}
+                  >
+                    {index + 1}
+                  </button>
+                </span>
+              ))}
+              <span>
+                <button
+                  onClick={() => {
+                    setPageInfo({
+                      ...pageInfo,
+                      curPage: Math.min(totalPage - 1, curPage + 1),
+                    })
+                  }}
+                  className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"></path>
+                  </svg>
+                </button>
+              </span>
+            </div>
+          </nav>
+        </div>
+      </div>
     )
   }
 
@@ -69,7 +167,7 @@ export default function ManagePosts() {
       <tr
         className="border-b last:border-none hover:bg-sky-100 hover:cursor-pointer"
         onClick={() => {
-          setSelectedPost(post)
+          setPageInfo({ ...pageInfo, selectedPost: post })
         }}
       >
         <td className="px-6 py-2">{post.id}</td>
