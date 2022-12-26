c63c8cf926bbef36caf8578e4b418cd3f69c895a
Jo-Jee <capjjo@gmail.com>
2022-12-26 16:02:45 +0900

    table 사이즈 full

--- src/pages/ManagePosts.tsx
+++ src/pages/ManagePosts.tsx
@@ -73,7 +73,7 @@ export default function ManagePosts() {
 
   function PostTable() {
     return (
-      <div>
+      <div className="w-full">
         <table className="table-auto w-full">
           <thead>
             <tr className="border-b">
