015cf149c4034b9b24f3e22c76073fed2fd1f7ff
Jo-Jee <capjjo@gmail.com>
2022-12-29 15:46:03 +0900

    Header에 email 표시하게 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,27 @@
 import { userActions } from '@slices/userSlice'
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
 import { removeToken } from '@utils/token'
+import { useAppDispatch, useAppSelector } from '@store'
 
 export default function Header() {
   const [showSetting, setShowSetting] = useState(false)
+  const { user } = useAppSelector((state) => state.user)
 
   const onProfileClick = () => {
     setShowSetting(!showSetting)
   }
   return (
     <div className="bg-white drop-shadow h-16 shrink-0 grow-0 flex justify-between items-center px-8 z-10">
-      <div>이거</div>
+      <div></div>
       <div
         className="flex items-center hover:cursor-pointer"
         onClick={onProfileClick}
       >
-        <img className="rounded-full w-10 h-10 bg-slate-200 p-1" src=""></img>
-        <div className="ml-3">userName</div>
+        <img
+          className="rounded-full w-10 h-10 bg-slate-200"
+          src="https://d11a6trkgmumsb.cloudfront.net/original/3X/d/8/d8b5d0a738295345ebd8934b859fa1fca1c8c6ad.jpeg"
+        ></img>
+        <div className="ml-3">{user?.email}</div>
       </div>
       {showSetting && <SettingPanel />}
     </div>
@@ -25,7 +29,7 @@ export default function Header() {
 }
 
 function SettingPanel() {
-  const dispatch = useDispatch()
+  const dispatch = useAppDispatch()
   const onLogout = () => {
     dispatch(userActions.clearUser())
     removeToken()
