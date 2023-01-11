5f7d63c7be0de0b54104ee32b939ba02c668edee
Jo-Jee <capjjo@gmail.com>
2023-01-11 12:16:36 +0900

    edit post id로 가져오게 변경

--- src/components/EditPost.tsx
+++ src/components/EditPost.tsx
@@ -15,14 +15,14 @@ interface EditPostProps {
 }
 
 export default function EditPost(props: EditPostProps) {
-  const [post, setPost] = useState<Post>(props.item)
+  const [post, setPost] = useState<Post>()
   const [topics, setTopics] = useState<Topic[]>([])
   const navigate = useNavigate()
 
   const onSubmit = async () => {
-    const method = post.id ? 'PUT' : 'POST'
+    const method = post?.id ? 'PUT' : 'POST'
 
-    await API.blog(`/posts${post.id ? `/${post.id}` : ''}`, {
+    await API.blog(`/posts${post?.id ? `/${post.id}` : ''}`, {
       method: method,
       data: post,
     })
@@ -40,124 +40,134 @@ export default function EditPost(props: EditPostProps) {
   }, [])
 
   useEffect(() => {
-    setPost(props.item)
+    const fetchPost = async () => {
+      const id = props.item.id
+
+      const res = await API.blog.get<Post>(`/posts/${id}`)
+
+      setPost(res.data)
+    }
+
+    fetchPost()
   }, [props])
 
-  return (
-    <div className="flex flex-col flex-1">
-      <div className="flex">
-        <span className="flex-1 m-1">
-          <Label>Title</Label>
-          <Input
-            type="text"
-            value={post.title}
-            onChange={(e) => {
-              setPost({
-                ...post,
-                title: e.target.value,
-              })
-            }}
-            autoFocus
-          />
-        </span>
-      </div>
-      <div className="flex">
-        <span className="flex-1 m-1">
-          <Label>Topic</Label>
-          <Select
-            value={post.topicId}
-            onChange={(e) => {
-              setPost({
-                ...post,
-                topicId: +e.target.value,
-              })
-            }}
-          >
-            <option value={0} disabled hidden>
-              토픽을 선택해주세요
-            </option>
-            {topics.map((topic) => {
-              return (
-                <option value={topic.id} key={topic.id}>
-                  {topic.name}
-                </option>
-              )
-            })}
-          </Select>
-        </span>
-        <span className="flex-1 m-1">
-          <Label>Tags</Label>
-          <Input
-            type="text"
-            value={post.tags}
-            onChange={(e) => {
-              setPost({
-                ...post,
-                tags: e.target.value.split(',').map((tag) => tag.trim()),
-              })
-            }}
-          />
-        </span>
-        <span className="m-1">
-          <Label>Published</Label>
-          <Toggle
-            checked={post.published}
-            onChange={(e) => {
-              setPost({
-                ...post,
-                published: e.target.checked,
-              })
-            }}
-          />
-        </span>
-      </div>
-      <div className="flex">
-        <span className="flex-1 m-1">
-          <Label>Summary</Label>
-          <Input
-            name="summary"
-            type="text"
-            value={post.summary}
-            onChange={(e) => {
-              setPost({
-                ...post,
-                summary: e.target.value,
-              })
-            }}
-          />
-        </span>
-      </div>
-      <div className="mt-3 flex flex-1">
-        <div className="flex w-1/2 p-1">
-          <textarea
-            name="body"
-            placeholder="Body"
-            value={post.body}
-            className="grow shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none"
-            onChange={(e) => {
-              setPost({ ...post, body: e.target.value })
-            }}
-          />
+  if (!post) return <div>Loading..</div>
+  else
+    return (
+      <div className="flex flex-col flex-1">
+        <div className="flex">
+          <span className="flex-1 m-1">
+            <Label>Title</Label>
+            <Input
+              type="text"
+              value={post.title}
+              onChange={(e) => {
+                setPost({
+                  ...post,
+                  title: e.target.value,
+                })
+              }}
+              autoFocus
+            />
+          </span>
         </div>
-        <div className="flex w-1/2 p-1 overflow-hidden">
-          <div className="flex flex-1 border shadow rounded overflow-scroll">
-            <ReactMarkdown
-              className="grow max-w-xl p-3 prose"
-              remarkPlugins={[remarkGfm]}
+        <div className="flex">
+          <span className="flex-1 m-1">
+            <Label>Topic</Label>
+            <Select
+              value={post.topicId}
+              onChange={(e) => {
+                setPost({
+                  ...post,
+                  topicId: +e.target.value,
+                })
+              }}
             >
-              {post.body}
-            </ReactMarkdown>
+              <option value={0} disabled hidden>
+                토픽을 선택해주세요
+              </option>
+              {topics.map((topic) => {
+                return (
+                  <option value={topic.id} key={topic.id}>
+                    {topic.name}
+                  </option>
+                )
+              })}
+            </Select>
+          </span>
+          <span className="flex-1 m-1">
+            <Label>Tags</Label>
+            <Input
+              type="text"
+              value={post.tags}
+              onChange={(e) => {
+                setPost({
+                  ...post,
+                  tags: e.target.value.split(',').map((tag) => tag.trim()),
+                })
+              }}
+            />
+          </span>
+          <span className="m-1">
+            <Label>Published</Label>
+            <Toggle
+              checked={post.published}
+              onChange={(e) => {
+                setPost({
+                  ...post,
+                  published: e.target.checked,
+                })
+              }}
+            />
+          </span>
+        </div>
+        <div className="flex">
+          <span className="flex-1 m-1">
+            <Label>Summary</Label>
+            <Input
+              name="summary"
+              type="text"
+              value={post.summary}
+              onChange={(e) => {
+                setPost({
+                  ...post,
+                  summary: e.target.value,
+                })
+              }}
+            />
+          </span>
+        </div>
+        <div className="mt-3 flex flex-1">
+          <div className="flex w-1/2 p-1">
+            <textarea
+              name="body"
+              placeholder="Body"
+              value={post.body}
+              className="grow shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none"
+              onChange={(e) => {
+                setPost({ ...post, body: e.target.value })
+              }}
+            />
+          </div>
+          <div className="flex w-1/2 p-1 overflow-hidden">
+            <div className="flex flex-1 border shadow rounded overflow-scroll">
+              <ReactMarkdown
+                className="grow max-w-xl p-3 prose"
+                remarkPlugins={[remarkGfm]}
+              >
+                {post.body}
+              </ReactMarkdown>
+            </div>
           </div>
         </div>
+        <div className="mt-3 flex justify-end px-3">
+          <button
+            className="bg-sky-900 text-white px-3 py-1 rounded"
+            onClick={onSubmit}
+          >
+            확인
+          </button>
+        </div>
       </div>
-      <div className="mt-3 flex justify-end px-3">
-        <button
-          className="bg-sky-900 text-white px-3 py-1 rounded"
-          onClick={onSubmit}
-        >
-          확인
-        </button>
-      </div>
-    </div>
-  )
+    )
 }
