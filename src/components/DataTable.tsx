e97d5ea8a1d5110c393d9df0f6fbf9a126a88163
Jo-Jee <capjjo@gmail.com>
2023-01-27 11:08:09 +0900

    publish 여부 svg로 변경

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,5 +1,5 @@
 import { Card } from '@components/Card'
-import { useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import { AxiosInstance } from 'axios'
 import { format, register } from 'timeago.js'
 import ko from 'timeago.js/lib/lang/ko'
@@ -244,10 +244,36 @@ export default function DataTable(props: Props) {
         }}
       >
         {Object.keys(tableHeadKeyTitle).map((key) => {
-          let content: string
+          let content: ReactNode
           switch (tableHeadKeyTitle[key].type) {
             case 'boolean':
-              content = item[key] ? '✅' : '❌'
+              content = item[key] ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 fill-green-500"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 fill-red-500"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              )
               break
             case 'date':
               content = format(item[key], 'ko')
